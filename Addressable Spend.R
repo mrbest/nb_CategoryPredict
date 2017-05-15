3e85a69ed377c4f3df4d72d228436e95ee835b91
bgiesy <beck.giesy@gmail.com>
2017-05-15 11:31:32 -0400

    Add files via upload

--- Addressable Spend.R
+++ Addressable Spend.R
@@ -12,8 +12,12 @@ main <- function()
   print("Loading data")
   fpds_df <- read_tsv("FPDS_ATOM_GW_19APR17.tsv")
   print("Generating training data")
-  testing_training_data <<- generate_training_data(fpds_df, "OS3")
-  new_proportions <<- proportional_spend(testing_training_data)
+  fpds_df <- fpds_df %>% filter(as.Date(date_signed) >= as.Date("2015-10-01") & as.Date(date_signed) <= as.Date("2016-09-30"))
+  testing_training_data <<- generate_training_data(fpds_df, "OASIS")
+  fpds_df$psc_naics_combo <- paste0(fpds_df$product_or_service_code, sep = "_", fpds_df$naics_code)
+  filtered_fpds <- filter(fpds_df, fpds_df$psc_naics_combo %in% testing_training_data$psc_naics_combo)
+  filtered_fpds_distinct <- distinct(filtered_fpds)
+  new_proportions <<- calculate_predicted_obligation(filtered_fpds_distinct)
 }
 
 generate_training_data <- function(transaction_df, bic_name)
@@ -24,15 +28,15 @@ generate_training_data <- function(transaction_df, bic_name)
   bic_ref_idv_piids <- vehicle_df %>% filter(Contract == bic_name) %>% select(`Contract ID`)%>% .$`Contract ID`
   transaction_df <- transaction_df %>% filter(reference_piid %in% bic_ref_idv_piids)
   transaction_df$psc_naics_combo <- paste0(transaction_df$product_or_service_code, sep = "_", transaction_df$naics_code)
-  proportions <<- proportional_spend(transaction_df)
-  transaction_df <- transaction_df %>% select(product_or_service_code, naics_code, funding_department_name, dollars_obligated, psc_naics_combo)
-  transaction_df <- na.omit(transaction_df)
+  proportions <<- produce_training_proportion(transaction_df)
+  transaction_df <- transaction_df %>% select(product_or_service_code, naics_code, funding_agency_name, dollars_obligated, psc_naics_combo)
+ # transaction_df <- na.omit(transaction_df)
   transaction_df
   }
 
-proportional_spend <- function(bic_df)
+produce_training_proportion <- function(bic_df)
 {
-  unique_psc_naics_bic_combo <- unique(bic_df$psc_naics_combo)
+  unique_psc_naics_bic_combo <<- unique(bic_df$psc_naics_combo)
   storage_df <- as.data.frame(NULL)
   i <- length(unique_psc_naics_bic_combo)
   value <- as.data.frame(NULL)
@@ -46,4 +50,14 @@ proportional_spend <- function(bic_df)
   }
   value$proportion <- value$`Dollars Obligated`/sum(bic_df$dollars_obligated)
   value
+}
+
+calculate_predicted_obligation <- function(matched_df)
+{
+  value <- sum(matched_df$dollars_obligated)
+  proportions_new <- distinct(filtered_fpds_distinct, psc_naics_combo, .keep_all = TRUE)
+  proportions_new$proportions <- proportions$proportion
+  proportions_new$Dollars_Predicted <- proportions_new$proportions * value
+  proportions_new <- proportions_new %>% select(psc_naics_combo, proportions, Dollars_Predicted)
+  proportions_new
 }
